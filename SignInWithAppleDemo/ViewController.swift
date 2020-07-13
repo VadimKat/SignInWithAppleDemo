453b595ee822beaa8358e67f4c985d997c83cb86
Vadym Katenin <vadimkatenin1@gmail.com>
2020-07-13 14:37:31 +0300

    Initial Commit

---
+++ SignInWithAppleDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignInWithAppleDemo
+//
+//  Created by Vadim Katenin on 13.07.2020.
+//  Copyright © 2020 Vadim Katenin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
